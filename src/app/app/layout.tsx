3eda569947fdeced9d03729a75c0fec15cb2646f
marieooq <otakimarie@gmail.com>
2025-01-20 12:11:47 +0900

    display image container only when a user is logged in

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import ImageInputOutputContainer from "@/components/imageInputOutput/imageInputOutputContainer";
-export default function Home() {
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log("data.user", data?.user);
+  if (error || !data?.user) {
+    redirect("/login");
+  }
   return (
     <div className="p-4">
+      <p>Hello, login user!</p>
       <ImageInputOutputContainer />
     </div>
   );
